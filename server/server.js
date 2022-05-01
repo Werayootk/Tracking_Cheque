51c0378aeb4080e4a9519426eda6267d350bf589
werayootK <werayoot5800@gmail.com>
2022-05-01 18:50:39 +0700

    Define Route API

---
+++ server/server.js
@@ -0,0 +1,24 @@
+/**
+ * Routes API ADMIN
+ * 1. GET /api/admin/users
+ * 2. GET /api/admin/users/:id
+ * 3. DELETE /api/admin/users/:id/delete
+ * 4. PUT /api/admin/users/:id/update
+ * 5. POST /api/admin/users/create
+ * 6. GET /api/companies
+ * 7. GET /api/companies/:id
+ * 8. POST /api/companies/create
+ * 9. PUT /api/companies/:id/update
+ * 10. DELETE /api/companies/:id/delete
+ * 11. GET /api/cheques
+ * 12. GET /api/cheques/:id
+ * 13. POST /api/cheques/create
+ * 14. PUT /api/cheques/:id/update
+ * 15. DELETE /api/cheques/:id/delete
+ */
+
+/**
+ * Routes API USER
+ * 16. GET /api/users/:id
+ * 17. GET /api/users/:id/cheques
+ */
